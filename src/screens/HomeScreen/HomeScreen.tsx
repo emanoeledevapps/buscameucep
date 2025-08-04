05a967945e733249ebc61afb8a70dab10dd38915
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-08-04 17:50:40 -0300

    add logo to home

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 
 import { Screen } from "@components";
 import { SearchArea } from "./components/SearchArea";
+import { Logo } from "./components/Logo";
 
 export function HomeScreen() {
 
@@ -10,6 +11,7 @@ export function HomeScreen() {
   return (
     <Screen hideHeader>
       <View className="flex-1 items-center justify-center p-5">
+        <Logo />
         <SearchArea />
       </View>
     </Screen>
---
+++ src/screens/HomeScreen/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { View, Image } from "react-native";
+//@ts-ignore
+import LogoApp from "../../../assets/images/icon.png";
+import { Text } from "@components";
+
+export function Logo() {
+  return (
+    <View className="flex-row w-full items-center justify-center mb-10">
+      <Image
+        source={LogoApp}
+        className="w-32 h-32"
+        resizeMode="contain"
+      />
+
+      <Text className="text-2xl text-text-primary font-semibold">Busca meu CEP</Text>
+    </View>
+  )
+}
