772a29c5b51c4ad7bbc3dba6204226834f1af331
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-08-04 09:40:57 -0300

    add option hide header

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -7,22 +7,28 @@ import { useAppSafeArea } from "@hooks";
 interface Props extends HeaderProps {
   children: ReactNode
   scrollable?: boolean
+  hideHeader?: boolean
 }
 
-export function Screen({ children, scrollable, ...headerProps }: Props) {
+export function Screen({ children, scrollable, hideHeader, ...headerProps }: Props) {
   const { top } = useAppSafeArea();
 
   if (scrollable) {
     return (
-      <ScrollView className="flex-1" style={{ paddingTop: top }}>
+      <ScrollView className="flex-1 bg-gray-200" style={{ paddingTop: top }}>
+        {!hideHeader && (
+          <Header {...headerProps} />
+        )}
         {children}
       </ScrollView>
     )
   }
 
   return (
-    <View className="flex-1" style={{ paddingTop: top }}>
-      <Header {...headerProps} />
+    <View className="flex-1 bg-gray-200" style={{ paddingTop: top }}>
+      {!hideHeader && (
+        <Header {...headerProps} />
+      )}
       {children}
     </View>
   )
