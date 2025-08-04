658502263a51520a3881d40673ac8c2bc0bea382
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-08-04 19:13:26 -0300

    change header height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ export function Header({ title, showBackButton }: HeaderProps) {
   }
 
   return (
-    <View className="w-full h-20 flex-row items-center justify-between px-5 border-b bg-primary border-text-secondary">
+    <View className="w-full h-[70px] flex-row items-center justify-between px-5 border-b bg-primary border-text-secondary">
       <View className="w-10">
         {showBackButton && (
           <TouchableOpacity className="w-10" onPress={handleGoBack}>
